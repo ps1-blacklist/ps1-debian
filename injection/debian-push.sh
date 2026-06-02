8f7f1ebd76f978907ed32082312a9281c0b34ed9
Proffesor BL4CKL1$T <160224539+ps1-blacklist@users.noreply.github.com>
2026-06-02 16:57:20 +0600

    Update debian-push.sh

--- injection/debian-push.sh
+++ injection/debian-push.sh
@@ -2,18 +2,20 @@
 
 RED='\e[0;31m'
 STOP='\e[0m' 
-distro=pd login debian
+distro="pd login debian"
+
 echo -e "\n${RED}​⚡ Checking debian graphics launcher...${STOP}\n"
 sleep 3
 clear
 
-if [[ ! -f ~/.x11-start.sh ]]; then
+if [[ ! $($distro -- bash -c 'test -f ~/.x11-start.sh; echo $?') -eq 0 ]]; then
     echo -e "\n${RED}​⚡ Injecting...${STOP}\n"
+
     $distro -- touch ~/.x11-start.sh
-    $distro -- echo "export DISPLAY=:0 && dbus-launch --exit-with-session startxfce4" > ~/.x11-start.sh
+    $distro -- bash -c 'echo "export DISPLAY=:0 && dbus-launch --exit-with-session startxfce4" > ~/.x11-start.sh'
+
     sleep 3
     echo -e "\n${RED}​⚡ Injected successfully...${STOP}\n"
-
 
 else
     echo -e "\n${RED}​⚡ Launcher already exist...${STOP}\n"
