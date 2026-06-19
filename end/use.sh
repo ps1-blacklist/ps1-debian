1f68ede7fa7ce08a98ea09383647ee849ec95db0
Proffesor BL4CKL1$T <160224539+ps1-blacklist@users.noreply.github.com>
2026-06-19 12:35:25 +0600

    Update use.sh

--- end/use.sh
+++ end/use.sh
@@ -2,4 +2,4 @@
 GREEN='\e[0;32m'
 STOP='\e[0m' 
 
-echo -e "\n${GREEN} Run in Termux \n\n     ps1-debian \n\n open Termux X11 and Enjoy${STOP}\n"
+echo -e "\n${GREEN} Restart Termux And Run  \n\n     ps1-debian \n\n open Termux X11 and Enjoy${STOP}\n"
